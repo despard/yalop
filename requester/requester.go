8b4c4b7b086fb515eaa2907cc8f4b4920c6b8f2d
liangzhaohao <641250162@qq.com>
2019-07-02 13:32:45 +0800

    压测

---
+++ requester/requester.go
@@ -0,0 +1,171 @@
+package requester
+
+import (
+	"io"
+	"net/url"
+	"os"
+	"sync"
+	"time"
+)
+
+// Max size of the buffer of result channel.
+const maxResult = 1000000
+const maxIdleConn = 500
+
+type IClient interface {
+	DoRequest() (error, int, int64)
+	DoClose() error
+}
+
+type result struct {
+	err           error
+	statusCode    int
+	offset        time.Duration
+	duration      time.Duration
+	connDuration  time.Duration // connection setup(DNS lookup + Dial up) duration
+	dnsDuration   time.Duration // dns lookup duration
+	reqDuration   time.Duration // request "write" duration
+	resDuration   time.Duration // response "read" duration
+	delayDuration time.Duration // delay between response and request
+	contentLength int64
+}
+
+type Work struct {
+	// Client is the client of request to be made.
+	Client IClient
+
+	// N is the total number of requests to make.
+	N int
+
+	// C is the concurrency level, the number of concurrent workers to run.
+	C int
+
+	// H2 is an option to make HTTP/2 requests
+	H2 bool
+
+	// Timeout in seconds.
+	Timeout int
+
+	// Qps is the rate limit in queries per second.
+	QPS float64
+
+	// Output represents the output type. If "csv" is provided, the
+	// output will be dumped as a csv stream.
+	Output string
+
+	// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
+	// Optional.
+	ProxyAddr *url.URL
+
+	// Writer is where results will be written. If nil, results are written to stdout.
+	Writer io.Writer
+
+	initOnce sync.Once
+	results  chan *result
+	stopCh   chan struct{}
+	start    time.Duration
+
+	report *report
+}
+
+func (b *Work) writer() io.Writer {
+	if b.Writer == nil {
+		return os.Stdout
+	}
+	return b.Writer
+}
+
+// Init initializes internal data-structures
+func (b *Work) Init() {
+	b.initOnce.Do(func() {
+		b.results = make(chan *result, min(b.C*1000, maxResult))
+		b.stopCh = make(chan struct{}, b.C)
+	})
+}
+
+// Run makes all the requests, prints the summary. It blocks until
+// all work is done.
+func (b *Work) Run() {
+	b.Init()
+	b.start = now()
+	b.report = newReport(b.writer(), b.results, b.Output, b.N)
+	// Run the reporter first, it polls the result channel until it is closed.
+	go func() {
+		runReporter(b.report)
+	}()
+	b.runWorkers()
+	b.Finish()
+}
+
+func (b *Work) Stop() {
+	// Send stop signal so that workers can stop gracefully.
+	for i := 0; i < b.C; i++ {
+		b.stopCh <- struct{}{}
+	}
+}
+
+func (b *Work) Finish() {
+	close(b.results)
+	b.Client.DoClose()
+	total := now() - b.start
+	// Wait until the reporter is done.
+	<-b.report.done
+	b.report.finalize(total)
+}
+
+func (b *Work) makeRequest(c IClient) {
+	var size int64
+	var code int
+	resStart := now()
+	err, code, size := c.DoRequest()
+	t := now()
+	resDuration := t - resStart
+	b.results <- &result{
+		offset:        resStart,
+		statusCode:    code,
+		duration:      resDuration,
+		err:           err,
+		contentLength: size,
+	}
+}
+
+func (b *Work) runWorker(client IClient, n int) {
+	var throttle <-chan time.Time
+	if b.QPS > 0 {
+		throttle = time.Tick(time.Duration(1e6/(b.QPS)) * time.Microsecond)
+	}
+
+	for i := 0; i < n; i++ {
+		// Check if application is stopped. Do not send into a closed channel.
+		select {
+		case <-b.stopCh:
+			return
+		default:
+			if b.QPS > 0 {
+				<-throttle
+			}
+			b.makeRequest(client)
+		}
+	}
+}
+
+func (b *Work) runWorkers() {
+	var wg sync.WaitGroup
+	wg.Add(b.C)
+
+	// Ignore the case where b.N % b.C != 0.
+	for i := 0; i < b.C; i++ {
+		go func() {
+			b.runWorker(b.Client, b.N/b.C)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
